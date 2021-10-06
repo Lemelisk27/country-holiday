ea32a9258078a39f00a426144168d8de110e92a4
Juan Jose Ramirez <jjramirez@miuandes.cl>
2021-10-06 11:45:10 -0700

    copied js

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,6 +1,5 @@
 var APIkey = "8ca77be93b8c15ee7b97c2c78186ba1507778311"
 var requestURL = "https://calendarific.com/api/v2/countries?&api_key=" + APIkey
-var requestHolidaysURL = 'https://calendarific.com/api/v2/holidays?&api_key=' + APIkey + '&country=US&year=2021'
 var countryList = {}
 var countyCodes = {}
 const BTN_STYLE = "waves-effect waves-light btn-large";
@@ -10,7 +9,12 @@ $("#add-country-form").on("submit", handleAddCountry);
 $("#add-country-btn").click(handleAddCountry);
 $("#countries-list").click(handleDeleteCountry);
 
-getAPI();
+onLoad();
+
+function onLoad() {
+    getAPI();
+    renderCountriesList();
+}
 
 function getAPI() {
     fetch(requestURL)
@@ -28,7 +32,6 @@ function getAPI() {
         })
 }
 
-
 // Method that handles the button click, or submit event for 
 // when a user adds a new country to the list of countries
 function handleAddCountry(e) {
@@ -37,30 +40,30 @@ function handleAddCountry(e) {
 
     // Getting local storage
     var countries = JSON.parse(localStorage.getItem("countries"));
-    if (countries === null) {
+    if(countries === null) {
         countries = [];
     }
 
     // Making sure input isn't a duplicate
-    if (!countries.includes(countryName)) {
+    if(!countries.includes(countryName) && countries.length < 3 && countryName in countryList) {
         countries.push(countryName);
         localStorage.setItem("countries", JSON.stringify(countries));
         renderCountriesList();
-    }
+    } 
 
     // Clear search bar
-    $("#countryI").val("");
+    $("#countryI").val(""); 
 }
 
 // Method that deletes a country from the list of countries a user has chosen
 function handleDeleteCountry(e) {
     var countryName = e.target.textContent;
-
+    
     // Getting local storage, and removing from local storage
     var countries = JSON.parse(localStorage.getItem("countries"));
-    if (countries !== null) {
-        for (let i = 0; i < countries.length; i++) {
-            if (countries[i].toLowerCase() === countryName.toLowerCase()) {
+    if(countries !== null) {
+        for(let i = 0; i < countries.length; i++) {
+            if(countries[i].toLowerCase() === countryName.toLowerCase()) {
                 countries.splice(i, 1);
                 localStorage.setItem("countries", JSON.stringify(countries));
                 break;
@@ -76,15 +79,15 @@ function renderCountriesList() {
     var list = document.getElementById("countries-list");
 
     // Before render, remove all children first
-    while (list.firstChild) {
+    while(list.firstChild) {
         list.removeChild(list.firstChild);
     }
 
     var countries = JSON.parse(localStorage.getItem("countries"));
 
     // Create the new li elements
-    if (countries !== null) {
-        for (let i = 0; i < countries.length; i++) {
+    if(countries !== null) {
+        for(let i = 0; i < countries.length; i++) {
             var newLi = $("<li>").html(countries[i]);
             $(newLi).addClass(BTN_STYLE).addClass("add-btn");
             $("#countries-list").prepend(newLi);
@@ -92,9 +95,9 @@ function renderCountriesList() {
     }
 }
 
-$(document).ready(function () {
+$(document).ready(function(){
     $('input.autocomplete').autocomplete({
-        data: countryList,
-        limit: 5,
+      data: countryList,
+      limit: 5,
     });
-});
+  });
